48280d8eb3fcc0693fe8366fa783cbb8a1ee8b52
Asrina Sibagariang <asrinasibagariang@gmail.com>
2024-10-27 23:31:56 +0700

    tampilan belanja dan dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/products', function () {
+    return view('products');
+});
+Route::get('/product', function () {
+    return view('product_detail');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
